0395e106e4a85fff9d319a3e0701371f7003b0c2
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 12:13:58 -0400

    add validations for meal attributes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,9 @@
 class Meal < ApplicationRecord
   has_many :plans
   has_many :users, through: :plans
+
+
+  validates :food, :day, :beverage, :beverage_ounces, presence: true
+  default_scope -> { order(updated_at: :desc)}
+
 end
