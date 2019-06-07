2f93a73f4dac70c28bcd9293822ecf2e353fc161
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 12:14:19 -0400

    add validations for plan attributes

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,8 @@
 class Plan < ApplicationRecord
   belongs_to :user
-  belongs_to :meal 
+  belongs_to :meal
+
+  validates :goal, :description, presence: true
+  default_scope -> { order(updated_at: :desc)}
+
 end
