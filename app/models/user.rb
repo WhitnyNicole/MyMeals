0039a13712fe0e61b3cbfa4127195a94cbb01d40
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 12:14:31 -0400

    add validations for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :plans
-  has_many :meals, through: :plans  
+  has_many :meals, through: :plans
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 
 end
