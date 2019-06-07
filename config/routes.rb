67cc7ffb872de3d71aa0a51c4c46dc1e27f0d862
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 13:05:15 -0400

    add routes for users, meals, plans

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
     post '/login' => 'sessions#create'
     delete '/logout' => 'sessions#destroy'
 
-
+      resources :users
+      resources :plans
+      resources :meals
+  
 end
